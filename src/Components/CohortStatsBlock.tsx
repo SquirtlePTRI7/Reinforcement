4670460231abb94413a4538d6360ac4bc9050867
elrjolliffe <elrjolliffe@gmail.com>
2022-11-26 10:34:48 -0800

    Updated CohortStatsBlock

--- src/Components/CohortStatsBlock.tsx
+++ src/Components/CohortStatsBlock.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
-
-import CohortStatsRow from './CohortStatsRow';
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const CohortStatsBlock = (props) => {
 
-  const [ allStats ] = props;
+  const [ cohortRows ] = props;
 
-  const Rows = allStats.map((user: Array<any>, index: number) => {
-    <CohortStatsRow key={index} user={user} />
-  })
   return (
-    { Rows }
+    <TableContainer component={Paper}>
+        <Table sx={{minWidth: 400}} aria-label='Leaderboard'>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Username</TableCell>
+                    <TableCell align='right'>Score</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {cohortRows.map((row: { key: number, username: string, score: number }) => (
+                    <TableRow key={row.key} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell component='th' scope='row'>{row.username}</TableCell>
+                        <TableCell align='right'>{row.score}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
   )
 }
 
--- src/Containers/CohortContainer.tsx
+++ src/Containers/CohortContainer.tsx
@@ -3,15 +3,24 @@ import React from 'react';
 import CohortStatsBlock from '../Components/CohortStatsBlock';
 
 const CohortContainer = () => {
-  const [allStats, setAllStats] = React.useState([]);
+  const [cohortRows, setCohortRows] = React.useState([]);
 
-  const getAllStats = () => {
+  const getCohortStats = () => {
     // DATABASE REQUEST AND SETALLSTATS
+
+    function createData (key: number, username: string, score: number){
+      return { key, username, score }
+    }
+
+    setCohortRows(data.map((profile: {username: string, score: number}, index: number) => {
+      createData(index, profile.username, profile.score)
+    }).sort((a,b) => a.profile.score - b.profile.score))
+  
   }
 
   return (
     <div>
-      <CohortStatsBlock allStats={allStats} />
+      <CohortStatsBlock allStats={cohortRows} />
     </div>
   )
 };
