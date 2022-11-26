f5732713b96fa0f84fd8d692d9e88989ce4bed3f
Glen <glen.kasoff@gmail.com>
2022-11-26 17:00:04 -0500

    backend user routes created and working

---
+++ server/controllers/userController.ts
@@ -0,0 +1,68 @@
+import { Application, Request, Response, NextFunction } from "express";
+const User = require("../model/model");
+import { UserController } from "../../types";
+
+const userController: UserController = {};
+
+userController.createUser = async (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+  });
+  try {
+    const newUser = await user.save();
+    res.locals.newUser = newUser;
+    return next();
+  } catch (err) {
+    return next({
+      message: "unable to CREATE user" + err,
+      log: `unable to CREATE user ` + err,
+    });
+  }
+};
+
+userController.getAllUsers = async (req, res, next) => {
+  console.log("getting all users...");
+  try {
+    const users = await User.find();
+    res.locals.users = users;
+    return next();
+  } catch (err) {
+    return next({
+      message: "unable to GET users",
+      log: "unable to GET users " + err,
+    });
+  }
+};
+
+userController.getOneUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    if (user === null) {
+      console.log("Cannot find user");
+      return res
+        .status(404)
+        .json({ message: "Cannot find user", log: "Cannot find user" });
+    } else {
+      res.locals.user = user;
+      return next();
+    }
+  } catch (err) {
+    return next({
+      message: "unable to GET one user",
+      log: "unable to GET one user " + err,
+    });
+  }
+};
+
+userController.deleteUser = async (req, res, next) => {
+  try {
+    await res.locals.user.remove();
+    return next();
+  } catch (err) {
+    return next({
+      message: "unable to DELETE user",
+      log: "unable to DELETE user " + err,
+    });
+  }
+};
+module.exports = userController;
---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+import { Application, Request, Response, NextFunction } from "express";
+
+router.post("/", userController.createUser, (req: Request, res: Response) => {
+  return res.status(200).json(res.locals.newUser);
+});
+
+router.get("/", userController.getAllUsers, (req: Request, res: Response) => {
+  return res.status(200).json(res.locals.users);
+});
+
+router.get(
+  "/:username",
+  userController.getOneUser,
+  (req: Request, res: Response) => {
+    console.log("Username found!");
+    return res.status(200).json(res.locals.user);
+  }
+);
+
+router.delete(
+  "/:username",
+  userController.getOneUser,
+  userController.deleteUser,
+  (req: Request, res: Response) => {
+    return res
+      .status(200)
+      .json({ message: res.locals.user.username + " has been deleted" });
+  }
+);
+
+module.exports = router;
