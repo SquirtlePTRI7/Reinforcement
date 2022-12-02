eff7b58fc4f0fb4a1486eb46bd37f7155c8093bc
elrjolliffe <elrjolliffe@gmail.com>
2022-12-01 18:19:45 -0800

    Fixed routes in server file

--- server/server.ts
+++ server/server.ts
@@ -1,85 +1,124 @@
-// const path = require("path");
-// const cors = require('cors')
-// const fetch = (...args: string[]) => {
-//   import('node-fetch').then(({default: fetch}) => fetch(...args))
-// }
-// const bodyParser = require('body-parser')
-// const express = require("express");
-// const app = express();
-// require("dotenv").config();
-// const mongoose = require("mongoose");
+const path = require("path");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const axios = require('axios')
+import { Application, Request, Response, NextFunction } from "express";
 
-// const PORT = process.env.PORT;
-// const MongoURI = process.env.MongoURI;
-// const CLIENT_ID = process.env.GITHUB_ID
-// const CLIENT_SECRET = process.env.GITHUB_SECRET
 
-// import { Application, Request, Response, NextFunction } from "express";
+const PORT = process.env.PORT;
+const MongoURI = process.env.MongoURI;
+const CLIENT_ID = process.env.GITHUB_ID
+const CLIENT_SECRET = process.env.GITHUB_SECRET
 
-// mongoose.connect(MongoURI);
-// const db = mongoose.connection;
-// db.on("error", () => console.log("Error connecting to Database"));
-// db.once("open", () => console.log("Connected to Database"));
+mongoose.connect(MongoURI);
+const db = mongoose.connection;
+db.on("error", () => console.log("Error connecting to Database"));
+db.once("open", () => console.log("Connected to Database"));
 
-// const DIST_DIR: string = path.join(__dirname, "../build/");
-// const HTML_FILE: string = path.join(DIST_DIR, "index.html");
+const DIST_DIR = path.join(__dirname, "../build/");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
 
-// // Serve static files
-// app.use(express.static(DIST_DIR));
-// app.use(express.static("../src/assets"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cors())
-// app.use(bodyParser.json())
+// Serve static files
+app.use(express.static(DIST_DIR));
+app.use(express.static("../src/assets"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(bodyParser.json())
 
-// app.use("/users", require("./routes/userRoutes"));
+app.use("/users", require("./routes/userRoutes"));
 
-// // Serve index.html
-// app.get("/*", (req: Request, res: Response) => {
-//   res.status(200).sendFile(HTML_FILE);
-// });
+//2nd implementation
+// app.get('/', (req, res) => {
+//   // The req.query object has the query params that were sent to this route.
+//   const requestToken = req.query.code
+//   console.log(requestToken)
+  
+//   axios({
+//     method: 'post',
+//     url: `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}`,
+//     // Set the content type header, so that we get the response in JSON
+//     headers: {
+//          accept: 'application/json'
+//     }    
+//   }).then((response) => {    
+//     const accessToken = response.data.access_token
+//     console.log(response.data)    
+//     // redirect the user to the home page, along with the access token
+//     res.redirect(`/?access_token=${accessToken}`)
+//   })
+// })
 
-// app.get('/getAccessToken', async function (req: Request, res: Response) {
-//   const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.query.code
+//1st implementation
+// app.get('/getAccessToken', async function (req, res) {
+//   const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=302a717b057f1cf52c20"
 //   await fetch("https://github.com/login/oauth/access_token" + params, {
-//     method: "POST",
-//     headers: {
-//       "Accept": "application/json"
-//     }
-//   }).then((res) => {
-//     return res.json()
-//   }).then((data) => {
-//     res.json(data)
-//   })
-// })
-
-// app.get('/getUserData', async function (req: Request, res: Response) {
-//   req.get("Authorization")
-//   await fetch("https://api.github.com/user", {
 //     method: "GET",
-//     headers: {
-//       "Authorizaiton": req.get("Authorization")
-//     }
-//   }).then((res) => {
-//     return res.json()
+//     // headers: {
+//     //   "Accept": "application/json"
+//     // }
+//   }).then((response) => {
+//     return response.json()
 //   }).then((data) => {
-//     res.json(data)
+//       console.log(data)
+//     return res.json(data)
+//   }).catch(err => {
+//     console.log('getAccessToken fetch error', err)
 //   })
 // })
 
-// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-//   const defaultErr = {
-//     log: "Express error handler caught unknown middleware error",
-//     status: 400,
-//     message: { err: "An error occurred" },
-//   };
-//   const errorObj = Object.assign({}, defaultErr, err);
-//   console.log(errorObj.log);
-//   return res.status(errorObj.status).json(errorObj.message);
-// });
 
-// // app.use("*", )
+app.post('/getAccessToken', async function (req: Request, res: Response) {
+  const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=2ae318765b195e7554cd"
+  // const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.body.code
+  await axios.post("https://github.com/login/oauth/access_token" + params
+  // , {
+    // method: "POST",
+    // headers: {
+    //   "Accept": "application/json"
+    // }
+  // }
+  ).then((response: {data: string}) => {
+      console.log(response)
+      console.log(typeof response.data)
+      return res.json(response.data.slice(13, 53))
+  }).catch((err: Error) => {
+    console.log('getAccessToken axios error', err)
+  })
+})
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening on Port ${PORT}`);
-// });
+app.get('/getUserData', async function (req: Request, res: Response) {
+  req.get("Authorization")
+  await axios.post("https://api.github.com/user", {
+    headers: {
+      "Authorization": req.get("Authorization")
+    }
+  }
+  ).then((data : any) => {
+    return res.json(data)
+  })
+})
+
+// Serve index.html
+app.get("/*", (req: Request, res: Response) => {
+  res.status(200).sendFile(HTML_FILE);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware error",
+    status: 400,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on Port ${PORT}`);
+});
